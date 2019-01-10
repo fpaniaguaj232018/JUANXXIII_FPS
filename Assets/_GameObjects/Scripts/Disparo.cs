66466ab4b0bc232ef7ba6037d203141a19a91076
fpaniaguaj232018 <fpaniaguaj232018@gmail.com>
2019-01-10 18:17:57 +0100

    Initial commit

---
+++ Assets/_GameObjects/Scripts/Disparo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour {
+
+    public GameObject prefabProyectil;
+    public Transform puntoGeneracion;
+    public float fuerzaDisparo = 0.1f;
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject proyectil =
+                Instantiate(
+                    prefabProyectil,
+                    puntoGeneracion.position,
+                    Quaternion.identity);
+            proyectil.GetComponent<Rigidbody>().AddForce(
+                transform.forward * fuerzaDisparo,
+                ForceMode.Impulse);
+            Destroy(proyectil, 3);
+        }
+	}
+}
