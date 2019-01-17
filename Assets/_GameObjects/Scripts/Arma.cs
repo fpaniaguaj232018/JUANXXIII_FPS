ac80dbfbf3c60cc939023a926202bc2b59e1f2db
fpaniaguaj232018 <fpaniaguaj232018@gmail.com>
2019-01-17 18:09:33 +0100

    Disparo y destruccion

--- Assets/_GameObjects/Scripts/Arma.cs
+++ Assets/_GameObjects/Scripts/Arma.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Disparo : MonoBehaviour {
-
+public class Arma : MonoBehaviour {
+    
+    public AudioClip disparo;//El fichero de sonido
+    private AudioSource audioSource;
     public GameObject prefabProyectil;
     public Transform puntoGeneracion;
     public float fuerzaDisparo = 0.1f;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
 	void Update () {
         if (Input.GetMouseButtonDown(0))
@@ -19,6 +26,7 @@ public class Disparo : MonoBehaviour {
             proyectil.GetComponent<Rigidbody>().AddForce(
                 transform.forward * fuerzaDisparo,
                 ForceMode.Impulse);
+            audioSource.PlayOneShot(disparo);
             Destroy(proyectil, 3);
         }
 	}
---
+++ Assets/_GameObjects/Scripts/Proyectil.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectil : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemigo"))
+        {
+            collision.gameObject.GetComponent<Enemigo>().HacerDanyo(1);
+        }
+    }
+}
