5152ebdf2ed19642b7c16fbba0a67609bf6ba770
fpaniaguaj232018 <fpaniaguaj232018@gmail.com>
2019-01-18 19:26:58 +0100

    Huesitos

---
+++ Assets/_GameObjects/Scripts/Huesitos.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Huesitos : MonoBehaviour {
+    Animator animator;
+    bool andando = false;
+    public float speed = 20;
+	void Start () {
+        animator = GetComponentInChildren<Animator>();
+        Invoke("Andar", 10);
+	}
+
+    void Update()
+    {
+        if (andando)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+	
+    void Andar()
+    {
+        andando = true;
+        animator.SetBool("andando", true);
+    }
+	
+}
