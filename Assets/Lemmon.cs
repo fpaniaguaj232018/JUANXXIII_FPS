52d9af820f67698dd6d4217df150d1efcf95da32
fpaniaguaj232018 <fpaniaguaj232018@gmail.com>
2019-01-31 18:17:07 +0100

    Barril con limon

---
+++ Assets/Lemmon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lemmon : MonoBehaviour {
+    float correccion = 0.001f;
+    float speed = 3;
+    float angularSpeed = 10;
+    float delta;
+    float y = 0;
+
+	void Update () {
+        y += Time.deltaTime*speed;
+        delta = Mathf.Cos(y)*correccion;
+        transform.Translate(new Vector3(0, delta, 0));
+        transform.Rotate(0, 1*Time.deltaTime*angularSpeed, 0);
+	}
+}
