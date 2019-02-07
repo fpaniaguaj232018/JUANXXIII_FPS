f1746f9a645e552af8b5010977620fe27afccaad
fpaniaguaj232018 <fpaniaguaj232018@gmail.com>
2019-02-07 20:15:53 +0100

    Enemigo ultralisto

---
+++ Assets/_GameObjects/Scripts/EnemigoUltralisto.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemigoUltralisto : MonoBehaviour {
+    public GameObject player;
+    private NavMeshAgent nma;
+	// Use this for initialization
+	void Start () {
+        nma = GetComponent<NavMeshAgent>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nma.destination = player.transform.position;
+    }
+}
